bcf8eb3bb8f63073ff33e6e67c482b9db6d26b59
Mark <mrodgers@cloudflare.com>
2023-07-03 21:13:07 +0100

    if statement for tls and non-tls

--- https_server.go
+++ https_server.go
@@ -12,10 +12,16 @@ func getData(w http.ResponseWriter, req *http.Request) {
 func main() {
   http.HandleFunc("/data", getData)
 
-  http.ListenAndServeTLS(
+  serverEnv := os.Getenv(SERVER_ENV)
+
+  if serverEnv == "DEV" {
+    http.ListenAndServe(":8080", nil)
+  } else if serverEnv == "PROD" {
+    http.ListenAndServeTLS(
     ":443",
     "/etc/letsencrypt/live/api.msrodgers.co.uk/fullchain.pem",
     "/etc/letsencrypt/live/api.msrodgers.co.uk/privkey.pem",
      nil,
     )
+  }  
 }
